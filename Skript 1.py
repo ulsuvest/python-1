78c56aee683ed9445cd45442b7d876d22da871cb
ulsuvest <118304525+ulsuvest@users.noreply.github.com>
2022-12-04 12:43:04 +0100

    Add files via upload

---
+++ Skript 1.py
@@ -0,0 +1,34 @@
+print("Hallo, liebe Welt")
+
+print("")
+x=3
+print (x)
+
+y=5.5
+print(y)
+z=x*y
+print("")
+print("x ist:")
+print (x)
+print ("y ist:")
+print(y)
+print ("x*y ist:")
+print(z)
+print("")
+print("Jetzt kannst du Zahlen bennenen")
+print("Gib 2 Zahlen ein und ich berechne das Produkt:")
+print("")
+a=input("a")
+print(a)
+print("")
+b=input("b")
+print(b)
+a=float(a)
+b=float(b)
+c=a*b
+print("")
+print("hier kommt das Ergebnis von a*b:")
+print("")
+print(c)
+print("")
+
